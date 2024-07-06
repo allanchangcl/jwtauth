3181ee5151f3b4854b553b48927af33c7fdf67f5
Chang CL <chang@cheeloy.com>
2024-07-06 22:54:47 +0800

    add database

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,9 +14,19 @@ type handler struct{}
 // Handler
 func (h *handler) helloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
+	// return c.String(http.StatusBadRequest, "Hello, World!")
 }
 
 func main() {
+	db, err := sql.Open("sqlite3", "./jwtauth.db") // not clear on what is needed for the user and password
+	if err != nil {
+		panic("Could not connect to the database")
+		// fmt.Println(err)
+		// fmt.Println("error one tripped")
+		// return
+	}
+	defer db.Close()
+
 	e := echo.New()
 	// e.GET("/", func(c echo.Context) error {
 	// 	return c.String(http.StatusOK, "Hello, world!")
